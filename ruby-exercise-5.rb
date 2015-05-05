53dd35f0d91fe4655aa576a020ab6fe0c2b63c77
learn06 <learn06@learn06.local>
2015-05-05 15:02:39 -0700

    Exercise 5 Finished

--- ruby-exercise-5.rb
+++ ruby-exercise-5.rb
@@ -1,5 +1,6 @@
 #When doing this exercise, use methods when appropriate to keep things organized.
 require 'set'
+require 'pp'
 
 snowy = File.read 'the-man-from-snowy-river.txt'
 clancy = File.read 'clancy-of-the-overflow.txt'
@@ -10,6 +11,11 @@ clancy_words = clancy.split /\W+/
 #A set is a data structure that can have things added and removed, and you can ask if things are in it. You can make one from an array
 dictionary = Set.new File.read('/usr/share/dict/words').lines.map{|w| w.chomp}
 
+names = []
+words.split(/\W+/).each do |w|
+  names << w if !w.capitalize!
+end
+names = Set.new names
 
 #We can treat arrays as sets, and use + for union, - for difference
 puts "Words in snowy but not clancy"
@@ -29,12 +35,15 @@ puts longest_word
 #Here, find the words that are in Snowy but not in the dictionary
 puts
 puts "Names in Snowy are:"
-
+n = names & snowy_words
+puts n.to_a.join ', '
 #Look up the documentation for Set to see how to test if a word is in it or not
 
 puts
 puts "Names in both Snowy and Clancy:"
-
+n = names & snowy_words
+n = n & clancy_words
+puts n.to_a.join ', '
 
 #Change this so it uses an array rather than a set. Marvel at how much slower it is.
 #You can time how long something takes to run in bash by doing time before the command.
